534a1069bb363c8afeb3f420f865056134fb17dc
Chris Christensen <christensenc@outlook.com>
2021-09-28 12:22:11 -0500

    Removing defaults

--- SharedCode/DNACModule.py
+++ SharedCode/DNACModule.py
@@ -7,21 +7,18 @@ logger = logging.getLogger()
 
 class DNAC():
 
-    def __init__(
-        self, username='devnetuser', password='Cisco123!', base_url='https://sandboxdnac.cisco.com:443', version='2.2.2.3'
-    ):
+    def __init__(self):
 
-        self.api = DNACenterAPI(username=username, password=password, base_url=base_url, version=version)
+        self.api = DNACenterAPI()
 
     def get_devices_for_card(self):
 
         device_list = self.api.devices.get_device_list()
 
-        return [{'hostname': x['hostname'], 'id': x['id']} for x in device_list['response']]
+        return [{'hostname': x['hostname'], 'id': x['id']} for x in device_list['response'] if x['hostname']]
 
     def get_device_details_for_card(self, d_id):
 
         d = self.api.devices.get_device_by_id(d_id)
 
         return d['response']
-
